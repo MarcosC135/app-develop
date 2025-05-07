2b464362d200562cc3208b1974862b190d88fca2
MarcosC135 <mecastellanoss@ufpso.edu.co>
2025-05-06 20:39:07 -0500

    nuevos cambios

---
+++ js/dashboardService.js
@@ -0,0 +1,139 @@
+function users(){
+    document.getElementById('cardHeader').innerHTML= '<h5>Listado de Usuarios </h5>'
+    const REQRES_ENDPOINT = 'https://reqres.in/api/users?page=1'
+
+    fetch(REQRES_ENDPOINT, {
+      method: 'GET' , 
+      headers: {
+        'Content-type' : 'application/json',
+        'x-api-key': 'reqres-free-v1'
+      } 
+    })
+    .then((response) =>{
+        return response.json().then(
+            data =>{
+                return{
+                    status: response.status,
+                    info: data
+                }
+            }
+        )
+    })
+
+    .then((result) =>{
+        console.log('resultado' , result)
+        if(result.status === 200){
+            let listUsers = `
+            <table class="table">
+            <thead>
+            <tr>
+            <th scope="col">#</th>
+            <th scope="col">Nombre</th>
+            <th scope="col">Apellido</th>
+            <th scope="col">Avatar</th>
+            </tr>
+            </thead>
+            <tbody>
+            `
+            result.info.data.forEach(element => {
+                listUsers = listUsers + `
+                <tr>
+                    <td>${element.id}</td>
+                    <td>${element.first_name}</td>
+                    <td>${element.last_name}</td>
+                    <td><img src="${element.avatar}" class="img-thumbnail" alt="avatar del usuario"></td>
+                </tr>
+                `
+            });
+            listUsers = listUsers + `
+              </tbody>
+            </table>
+            `
+            document.getElementById('info').innerHTML = listUsers
+        }else{
+            document.getElementById('info').innerHTML = 'No existen usuarios'
+        }
+    })
+}
+
+
+function products(){
+    document.getElementById('cardHeader').innerHTML= '<h5>Listado de Productos </h5>'
+    const REQRES_ENDPOINT = 'https://reqres.in/api/products?page=1'
+
+    fetch(REQRES_ENDPOINT, {
+      method: 'GET' , 
+      headers: {
+        'Content-type' : 'application/json',
+        'x-api-key': 'reqres-free-v1'
+      } 
+    })
+    .then((response) =>{
+        return response.json().then(
+            data =>{
+                return{
+                    status: response.status,
+                    info: data
+                }
+            }
+        )
+    })
+
+    .then((result) =>{
+        console.log('resultado' , result)
+        if(result.status === 200){
+            let listProduct = `
+            <table class="table">
+            <thead>
+            <tr>
+            <th scope="col">#</th>
+            <th scope="col">Producto</th>
+            <th scope="col">Año</th>
+            <th scope="col">Color</th>
+            </tr>
+            </thead>
+            <tbody>
+            `
+            result.info.data.forEach(element => {
+                listProduct = listProduct + `
+                <tr>
+                    <td>${element.id}</td>
+                    <td>${element.name}</td>
+                    <td>${element.year}</td>
+                    <td><input type='color' value='${element.color}'></td>
+                </tr>
+                `
+            });
+            listProduct = listProduct + `
+              </tbody>
+            </table>
+            `
+            document.getElementById('info').innerHTML = listProduct
+        }else{
+            document.getElementById('info').innerHTML = 'No existen usuarios'
+        }
+    })
+}
+
+
+
+function logout() {
+    localStorage.removeItem('token');
+    
+    // Opcionalmente, limpiar cualquier otro dato de sesión
+    // localStorage.removeItem('user');
+    
+    // Mostrar mensaje de cierre de sesión (opcional)
+    const infoElement = document.getElementById('info');
+    if (infoElement) {
+        infoElement.innerHTML = '<div class="alert alert-success">Sesión cerrada correctamente. Redirigiendo...</div>';
+    }
+    
+    // Redirigir después de un breve retraso para que el usuario vea el mensaje
+    setTimeout(() => {
+        location.href = '../index.html';
+    }, 1500);
+    
+    // También puedes agregar un evento para registrar el cierre de sesión
+    console.log('Usuario ha cerrado sesión: ' + new Date().toLocaleString());
+}
